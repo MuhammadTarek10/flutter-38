2d611d2e881aae0c7e72a38bcd369914cc9560bd
Muhammad Tarek <muhammadtarek@Muhammads-MacBook-Air.local>
2022-04-19 20:12:35 +0200

    removed some unused imports

--- mynotes/lib/views/register_view.dart
+++ mynotes/lib/views/register_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/exceptions/auth_exceptions.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
@@ -7,7 +6,6 @@ import 'package:mynotes/services/auth/bloc/auth_event.dart';
 import 'package:mynotes/services/auth/bloc/auth_state.dart';
 import 'package:mynotes/utilities/dialogs/error_dialog.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'dart:developer' as devtools show log;
 
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
